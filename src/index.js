aae900f37be93ef1e982f73cb8ff36a25409545e
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-02-06 18:01:53 -0500

    displayed app to root

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Scale from './components/Scale';
 
-function tick() {
-    const element = (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {new Date().toLocaleTimeString()}.</h2>
-      </div>
-    );
-    ReactDOM.render(element, document.getElementById('root'));
-}
-
-setInterval(tick, 1000);
+const element = (
+  <>
+    <Scale />
+    <button className="new-scale" >+</button>
+    <div className="description">
+      <h1>What is this tool?</h1>
+      <p>
+        This is a tool that helps you evaluate how you feel about the possibility of acheiving your goals.
+      </p>
+      <h1>Instructions</h1>
+      <p>
+        <ul>
+          <li>
+            Press the orange button to create a new scale.
+          </li>
+          <li>
+            Write the name of the goal you want to achieve.
+          </li>
+          <li>
+            Move the scale in the direction that you feel is correct for how you feel about the possibility of acheiving your goal
+          </li>
+        </ul>
+      </p>
+    </div>
+  </>
+)
+ReactDOM.render(element, document.getElementById('root'));
