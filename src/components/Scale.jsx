d92863051ebc2af909fc6b0f3b691329dfd282d6
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-02-07 15:17:56 -0500

    added updated svg

--- src/components/Scale.jsx
+++ src/components/Scale.jsx
@@ -3,14 +3,18 @@ import ScaleTitle from './ScaleTitle';
 import ScaleSlider from './ScaleSlider';
 import WritingSpace  from "./WritingSpace";
 
+//SVG
+import ExplanationIconClosed from "./icons/ExplanationIconClosed";
+import DeleteIcon from "./icons/DeleteIcon";
+
 export default class Scale extends Component {
     render() {
         return (
             <div className="scale">
                 <div className="scale__header">
                     <ScaleTitle />
-                    <img src={"./componenet-icons/explanationIcon-closed.svg"} alt="Explanation Button"/>
-                    <img src={"./componenet-icons/deleteIcon.svg"} alt="Delete Button"/> 
+                    <ExplanationIconClosed alt="Explanation Button (Closed)"/>
+                    <DeleteIcon alt="Delete Button"/> 
                 </div>
                 <ScaleSlider />
                 <WritingSpace />
--- src/components/ScaleTitle.jsx
+++ src/components/ScaleTitle.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
+//SVG
+import EditIcon from "./icons/EditIcon";
+
 export default class ScaleTitle extends Component {
     render() {
         return (
             <>
                 <input type="text" className="scale__header__input" placeholder="Name of Goal" required/>
-                <img src={"./component-icons/editIcon.svg"} alt="Edit Button" />
+                <EditIcon alt="Edit Button" />
             </>
         )
     }
