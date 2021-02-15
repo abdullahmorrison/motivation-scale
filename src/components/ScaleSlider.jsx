dea6e8d30132089a69983e7942494a513f81264c
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-02-15 07:23:56 -0500

    update input range state

--- src/components/ScaleSlider.jsx
+++ src/components/ScaleSlider.jsx
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 
 export default class ScaleSlider extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            sliderValue: 50
+        };
+    }
+    componentDidUpdate(prevState){
+        if(this.state.sliderValue !== prevState.sliderValue){
+            this.setState({sliderValue: this.state.sliderValue})
+        }
+    }
+
     render() {
         return (
             <div className="scale__slider">
                 <input
                     type="range"
                     className="scale__slider__range"
+                    min="0" max="100" 
+                    onChange={()=>this.setState({sliderValue: this.value})}
                 />
                 <div className="scale__slider__ticks">
                     <div></div>
