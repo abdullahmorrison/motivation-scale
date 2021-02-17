4385adf191297cbd93247087eefc16a0ec8d114a
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-02-17 08:44:33 -0500

    working delete button

--- src/components/Scale.jsx
+++ src/components/Scale.jsx
@@ -8,13 +8,14 @@ import ExplanationIconClosed from "./icons/ExplanationIconClosed";
 import DeleteIcon from "./icons/DeleteIcon";
 
 export default class Scale extends Component {
+
     render() {
         return (
             <div className="scale">
                 <div className="scale__header">
                     <ScaleTitle />
                     <ExplanationIconClosed alt="Explanation Button (Closed)"/>
-                    <DeleteIcon alt="Delete Button"/> 
+                    <DeleteIcon alt="Delete Button" onClick={()=>this.props.onDelete(this.props.id)}/> 
                 </div>
                 <ScaleSlider />
                 <WritingSpace />
