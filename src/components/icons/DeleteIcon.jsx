e74ee3e944d3f7869bc019ee3744620443c5d329
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-02-20 08:53:38 -0500

    deleteIcon early styling

--- src/components/icons/DeleteIcon.jsx
+++ src/components/icons/DeleteIcon.jsx
@@ -3,6 +3,7 @@ import * as React from "react"
 function DeleteIcon(props) {
   return (
     <svg
+      className="scale__header__deleteIcon"
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
       width={35}
