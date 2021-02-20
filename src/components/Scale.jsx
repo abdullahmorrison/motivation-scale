221f1b467b6f6581aa876851f057bb5f8057a86c
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-02-20 08:43:18 -0500

    writing space expanding and retract functionality

--- src/components/Scale.jsx
+++ src/components/Scale.jsx
@@ -8,17 +8,27 @@ import ExplanationIconClosed from "./icons/ExplanationIconClosed";
 import DeleteIcon from "./icons/DeleteIcon";
 
 export default class Scale extends Component {
+    state = {
+        writingSpace: null
+    }
+    handleWritingSpace = () =>{ //displays writing space visble or removes it
+        if(this.state.writingSpace === null){
+            this.setState({writingSpace: <WritingSpace/>})
+        }else{
+            this.setState({writingSpace: null})
+        }
+    }
 
     render() {
         return (
             <div className="scale">
                 <div className="scale__header">
                     <ScaleTitle />
-                    <ExplanationIconClosed alt="Explanation Button (Closed)"/>
+                    <ExplanationIconClosed alt="Explanation Button (Closed)" onClick={this.handleWritingSpace}/>
                     <DeleteIcon alt="Delete Button"  onClick={()=>this.props.onDelete(this.props.scale.id)}/> 
                 </div>
                 <ScaleSlider />
-                <WritingSpace />
+                {this.state.writingSpace}
             </div>
         )
     }
