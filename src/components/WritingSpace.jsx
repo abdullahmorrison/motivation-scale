c812b3f09a2ec1cc930f4c89148e0a9f26f9e7fc
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-02-21 20:56:55 -0500

    writing space layout

--- src/components/WritingSpace.jsx
+++ src/components/WritingSpace.jsx
@@ -3,16 +3,33 @@ import React, { Component } from 'react'
 export default class WritingSpace extends Component {
     render() {
         return (
-            <div>
+            <div className="scale__writing-space">
                 <div>
-                    <label>Explanation</label>
-                    <input type="textarea"/>
+                    <label><h2>Explanation</h2></label>
+                    <textarea 
+                        defaultValue="Enter your comment here..." 
+                        onFocus={
+                            (event)=>{
+                                event.target.value='';
+                                event.target.style.color='black';
+                            }
+                        }
+                    />
                 </div>
                 <div>
-                    <label>Future Plan</label>
-                    <input type="textarea"/>
+                    <label><h2>Future Plan</h2></label>
+                    <textarea 
+                        defaultValue="Enter your comment here..." 
+                        onFocus={
+                            (event)=>{
+                                event.target.value='';
+                                event.target.style.color='black';
+                            }
+                        }
+                    />
                 </div>
             </div>
+            
         )
     }
 }
