8e4e1636c1bfa41693bb7bdc4815ce971e8b3fed
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-03-10 07:03:33 -0500

    saving slider values to local storage while connecting to parent id

--- src/components/ScaleSlider.jsx
+++ src/components/ScaleSlider.jsx
@@ -3,9 +3,22 @@ import React, { Component } from 'react';
 export default class ScaleSlider extends Component {
     state = {
         sliderValue: 50
-    };
+    }
+    
+    componentDidMount() {//adding the saved scales from local storage to state
+        const sliderValue = JSON.parse(localStorage.getItem("sliderValue-"+this.props.scaleID))
+        if(sliderValue){ //if you can't find the item on local storage
+            this.setState({
+                sliderValue
+            })
+        }
+    }
     changeSliderValue = (value) =>{
         this.setState({sliderValue: value})
+        localStorage.setItem("sliderValue-"+this.props.scaleID, JSON.stringify(value))
+    }
+    componentWillUnmount(){
+        localStorage.removeItem("sliderValue-"+this.props.scaleID)
     }
 
     render() {
@@ -15,7 +28,7 @@ export default class ScaleSlider extends Component {
                     type="range"
                     className="scale__slider__range"
                     min="0" max="100" 
-                    value = {this.state.sliderValue}
+                    defaultValue = {this.state.sliderValue}
                     onChange = {(event) => this.changeSliderValue(event.target.value)}
                 />
                 <div className="scale__slider__ticks">
