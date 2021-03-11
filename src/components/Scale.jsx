86e66c07c374d9154348e64f139baa441b16b14e
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-03-10 19:39:21 -0500

    added unmount for deleting writing data

--- src/components/Scale.jsx
+++ src/components/Scale.jsx
@@ -15,17 +15,22 @@ export default class Scale extends Component {
         this.setState({writingSpaceVisible: !this.state.writingSpaceVisible})
     }
 
+    componentWillUnmount(){
+        localStorage.removeItem("scaleExplanation-"+this.props.scale.id)
+        localStorage.removeItem("scaleFuturePlan-"+this.props.scale.id)
+    }
+
     render() {
         return (
             <div className="scale">
                 <div className="scale__header">
-                    <ScaleTitle />
+                    <ScaleTitle scaleID={this.props.scale.id}/>
                     <ExplanationIconClosed alt="Explanation Button (Closed)" onClick={this.handleWritingSpace}/>
                     <DeleteIcon alt="Delete Button"  onClick={()=>this.props.onDelete(this.props.scale.id)}/> 
                 </div>
-                <ScaleSlider />
+                <ScaleSlider scaleID={this.props.scale.id}/>
                 {
-                    this.state.writingSpaceVisible === true ? <WritingSpace/>: null
+                    this.state.writingSpaceVisible === true ? <WritingSpace scaleID={this.props.scale.id}/>: null
                 }
             </div>
         )
