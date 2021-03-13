1037570a17d40088644d7a63c4e0eb7a0160e2e5
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-03-13 17:30:17 -0500

    class component to function with hooks conversion

--- src/components/ScaleTitle.jsx
+++ src/components/ScaleTitle.jsx
@@ -1,53 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react'
 
 //SVG
-import EditIcon from "./icons/EditIcon";
+import EditIcon from "./icons/EditIcon"
 
 
-export default class ScaleTitle extends Component {
-    state = {
-        value: null, //the title
-        displayH1: false //used to determine if you want to display value as h1 or input
-    };
-    componentDidMount() {//adding the saved scales from local storage to state
-        const scaleTitle = JSON.parse(localStorage.getItem("scaleTitle-"+this.props.scaleID))
+const ScaleTitle = props => {
+    const [value, setValue] = useState(null) //the title
+    const [displayH1, setDisplayH1] = useState(false) //used to determine if you want to display value as h1 or input
+
+    useEffect(()=>{
+        //*componentDidMount
+        //adding the saved scales from local storage to state
+        const scaleTitle = JSON.parse(localStorage.getItem("scaleTitle-"+props.scaleID))
         if(scaleTitle){ //if you can't find the item on local storage
-            this.setState({
-                value: scaleTitle,
-                displayH1: true
-            })
+            setValue(scaleTitle)
+            setDisplayH1(true)
         }
-    }
-    handleTitleChange = (event, value) =>{
+
+        //*componentWillUnmount
+        return localStorage.removeItem("scaleTitle-"+props.scaleID) 
+    }, [props.scaleID])
+
+    const handleTitleChange = (event, value) =>{
         if(event.key === 'Enter'){//if you press the enter key
-            this.setState({value: value, displayH1: true})
-            localStorage.setItem("scaleTitle-"+this.props.scaleID, JSON.stringify(value))
+            setValue(value)
+            setDisplayH1(true)
+            localStorage.setItem("scaleTitle-"+props.scaleID, JSON.stringify(value))
         }
     }
-    handleEditTitle = () =>{
-        this.setState({displayH1: false})
+    const handleEditTitle = () =>{
+        setDisplayH1(false)
     }
-    componentWillUnmount(){
-        localStorage.removeItem("scaleTitle-"+this.props.scaleID)
-    }
-    render() {
-        return (
-            <>
-                { this.state.displayH1 === true 
-                    ? <h1>{this.state.value}</h1>
-                    :<input 
-                        type="text" 
-                        className="scale__header__input" 
-                        defaultValue={this.state.value} 
-                        placeholder="Name of Goal" 
-                        onKeyDown={(event)=>this.handleTitleChange(event, event.target.value)}   
-                    />
-                }
-                <EditIcon 
-                    alt="Edit Button" 
-                    onClick={this.handleEditTitle}
+
+    return (
+        <>
+            { displayH1 === true 
+                ? <h1>{value}</h1>
+                :<input 
+                    type="text" 
+                    className="scale__header__input" 
+                    defaultValue={value} 
+                    placeholder="Name of Goal" 
+                    onKeyDown={(event)=>handleTitleChange(event, event.target.value)}   
                 />
-            </>
-        )
-    }
+            }
+            <EditIcon 
+                alt="Edit Button" 
+                onClick={handleEditTitle}
+            />
+        </>
+    ) 
 }
+export default ScaleTitle
