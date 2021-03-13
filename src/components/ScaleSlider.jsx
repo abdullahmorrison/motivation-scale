0fdc46f3f2b7f702be767ee2aa7fb14fd2293d61
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-03-13 17:39:38 -0500

    class component to function hooks conversion

--- src/components/ScaleSlider.jsx
+++ src/components/ScaleSlider.jsx
@@ -1,49 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default class ScaleSlider extends Component {
-    state = {
-        sliderValue: 50
-    }
+const ScaleSlider = props => {
+    const [sliderValue, setSliderValue] = useState(50)
     
-    componentDidMount() {//adding the saved scales from local storage to state
-        const sliderValue = JSON.parse(localStorage.getItem("sliderValue-"+this.props.scaleID))
+    useEffect(()=>{
+        //*compoenentDidMount
+        //adding the saved scales from local storage to state
+        const sliderValue = JSON.parse(localStorage.getItem("sliderValue-"+props.scaleID))
         if(sliderValue){ //if you can't find the item on local storage
-            this.setState({
-                sliderValue
-            })
+            setSliderValue(sliderValue)
         }
-    }
-    changeSliderValue = (value) =>{
-        this.setState({sliderValue: value})
-        localStorage.setItem("sliderValue-"+this.props.scaleID, JSON.stringify(value))
-    }
-    componentWillUnmount(){
-        localStorage.removeItem("sliderValue-"+this.props.scaleID)
+        //*componentWillUnmount
+        return localStorage.removeItem("sliderValue-"+props.scaleID)
+    },[props.scaleID])
+
+    const changeSliderValue = (value) =>{
+        setSliderValue(value)
+        localStorage.setItem("sliderValue-"+props.scaleID, JSON.stringify(value))
     }
 
-    render() {
-        return (
-            <div className="scale__slider">
-                <input
-                    type="range"
-                    className="scale__slider__range"
-                    min="0" max="100" 
-                    defaultValue = {this.state.sliderValue}
-                    onChange = {(event) => this.changeSliderValue(event.target.value)}
-                />
-                <div className="scale__slider__ticks">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-                <ul className="scale__slider__labels">
-                    <li>Saving What You Can</li>
-                    <li>Avoiding Failure</li>
-                    <li>Stagnant</li>
-                    <li>Chasing Success</li>
-                    <li>Upgrading Your Goal</li>
-                </ul>
+    return (
+        <div className="scale__slider">
+            <input
+                type="range"
+                className="scale__slider__range"
+                min="0" max="100" 
+                defaultValue = {sliderValue}
+                onChange = {(event) => changeSliderValue(event.target.value)}
+            />
+            <div className="scale__slider__ticks">
+                <div></div>
+                <div></div>
+                <div></div>
             </div>
-        )
-    }
+            <ul className="scale__slider__labels">
+                <li>Saving What You Can</li>
+                <li>Avoiding Failure</li>
+                <li>Stagnant</li>
+                <li>Chasing Success</li>
+                <li>Upgrading Your Goal</li>
+            </ul>
+        </div>
+    )
 }
+export default ScaleSlider
