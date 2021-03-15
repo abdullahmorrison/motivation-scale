7a0459b5b21c2e5e75895c2e2b378986599df72b
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-03-15 17:57:58 -0400

    class component to functional with hooks conversion

--- src/components/PGPScale.jsx
+++ src/components/PGPScale.jsx
@@ -1,62 +1,60 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Scale from './Scale';
 
 //packages
 import { v4 as uuidv4 } from 'uuid';
 
-class GoalProgress extends Component {
-    state = { 
-        scales: [] 
-    }
-    componentDidMount() {//adding the saved scales from local storage to state
+const PGPScale = () => {
+    const [scales, setScales] = useState([])
+
+    useEffect(()=>{
+        //*componentDidMount
+        //adding the saved scales from local storage to state
         const scales = JSON.parse(localStorage.getItem("scales"))
         if(scales !== null){ //if you can't find the item on local storage
-            this.setState({
-                scales
-            })
+            setScales(scales)
         }
+    }, [])
+
+    const handleAddScale = () =>{ //saving new scale to state and local storage
+        const localScales = [...scales, {id: uuidv4()}];
+        setScales(localScales)
+        localStorage.setItem("scales", JSON.stringify(localScales))
     }
-    handleAddScale = () =>{ //saving new scale to state and local storage
-        const scales = [...this.state.scales, {id: uuidv4()}];
-        this.setState({ scales })
-        localStorage.setItem("scales", JSON.stringify(scales))
-    }
-    handleDeleteScale = scaleId =>{
+    const handleDeleteScale = scaleId =>{
         //removing the scale from state and local storage by creating a new set of scales without the on we want to remove
-        const scales = this.state.scales.filter(s => s.id !== scaleId)
-        this.setState({ scales })
-        localStorage.setItem("scales", JSON.stringify(scales))
+        const localScales = scales.filter(s => s.id !== scaleId)
+        setScales(localScales)
+        localStorage.setItem("scales", JSON.stringify(localScales))
     }
-    render() { 
-        return ( 
-            <>
-                {this.state.scales.map(scale =>(
-                    <Scale key={scale.id} scaleID={scale.id} onDelete={this.handleDeleteScale}/>
-                ))}
-                <button className="new-scale" onClick={this.handleAddScale}>+</button>
-                <div className="description">
-                <h1>What is this tool?</h1>
-                <p>
-                    This is a tool that helps you evaluate how you feel about the possibility of acheiving your goals.
-                </p>
-                <h1>Instructions</h1>
-                <p>
-                    <ul>
-                    <li>
-                        Press the orange button to create a new scale.
-                    </li>
-                    <li>
-                        Write the name of the goal you want to achieve.
-                    </li>
-                    <li>
-                        Move the scale in the direction that you feel is correct for how you feel about the possibility of acheiving your goal
-                    </li>
-                    </ul>
-                </p>
-                </div>
-            </>
-        );
-    }
+    
+    return ( 
+        <>
+            {scales.map(scale =>(
+                <Scale key={scale.id} scaleID={scale.id} onDelete={handleDeleteScale}/>
+            ))}
+            <button className="new-scale" onClick={handleAddScale}>+</button>
+            <div className="description">
+            <h1>What is this tool?</h1>
+            <p>
+                This is a tool that helps you evaluate how you feel about the possibility of acheiving your goals.
+            </p>
+            <h1>Instructions</h1>
+            <p>
+                <ul>
+                <li>
+                    Press the orange button to create a new scale.
+                </li>
+                <li>
+                    Write the name of the goal you want to achieve.
+                </li>
+                <li>
+                    Move the scale in the direction that you feel is correct for how you feel about the possibility of acheiving your goal
+                </li>
+                </ul>
+            </p>
+            </div>
+        </>
+    );   
 }
- 
-export default GoalProgress;
+export default PGPScale;
