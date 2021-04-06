72dc6f739bced24cbd1296df3fda354beb14353f
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-04-06 19:18:47 -0400

    connected to database

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+//connect to db
+const mongoose = require('mongoose');
+//security (dotenv)
+require('dotenv/config')
 
 //cors = cross-origin resource sharing
 const cors = require('cors');
@@ -7,16 +11,21 @@ app.use(cors({origin: 'http://localhost:3000'}))
 
 app.get('/api/scales', (req, res)=>{
     const scales = [
-            {
-                id: 1, 
-                title: 'Coding the PGP Scale', 
-                sliderValue: 50, 
-                explanation: '',
-                futurePlan: ''
-            }
+        {
+            id: 1,
+            title: 'Coding the PGP Scale',
+            sliderValue: 60,
+            explanation: "Making great progress",
+            futurePlan: "Continue working hard :)"
+        },
+        {id: 2}
     ];
     res.json(scales);
 })
 
-const port = 3001;
-app.listen(port, ()=>console.log(`Server started on port ${port}`))
+const port = process.env.PORT || 3001;
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=>{app.listen(port, ()=>console.log(`Server started on port ${port}`))})
+    .catch((error)=> console.log(error.message))
+    
+mongoose.set('useFindAndModify', false)
