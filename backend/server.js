d1c18bd678320d79b18edc514b641b4851547e11
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-04-12 17:49:56 -0400

    database get, post, patch, delete functionality

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,57 @@
 const express = require('express');
 const app = express();
-//connect to db
-const mongoose = require('mongoose');
-//security (dotenv)
-require('dotenv/config')
+const mongoose = require('mongoose');//connect to db
+const bodyParser = require('body-parser')
+const Scale = require('./models/scale');
+require('dotenv/config')//security (dotenv)
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
 
 //cors = cross-origin resource sharing
 const cors = require('cors');
 app.use(cors({origin: 'http://localhost:3000'}))
 
-app.get('/api/scales', (req, res)=>{
-    const scales = [
-        {
-            id: 1,
-            title: 'Coding the PGP Scale',
-            sliderValue: 60,
-            explanation: "Making great progress",
-            futurePlan: "Continue working hard :)"
-        },
-        {id: 2}
-    ];
-    res.json(scales);
+app.get('/api/scales', async (req, res)=>{ //retrieving data
+    try {
+        const postMessages = await Scale.find();
+                
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+});
+
+app.post('/api/scales', async (req, res)=>{ //adding data
+    const scale = new Scale({
+        title: req.body.title,
+        sliderValue: req.body.sliderValue,
+        explanation: req.body.explanation,
+        futurePlan: req.body.futurePlan
+    })
+    try {
+        const savedScale = await scale.save();
+        res.status(201).json(savedScale);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+})
+
+app.patch('/api/scales/:scaleID', async (req, res)=>{//updating data
+    try{
+        const updatedScale = await Scale.updateOne({_id: req.params.scaleID})
+        res.json(updatedScale)
+    }catch(err){
+        res.json({message: err})
+    }
+})
+
+app.delete('/api/scales/:scaleID', async (req, res)=>{//deleting data
+    try{
+        const removedScale = await Scale.deleteOne({_id: req.params.scaleID})
+        res.json(removedScale)
+    }catch(err){
+        res.json({message: err})
+    }
 })
 
 const port = process.env.PORT || 3001;
