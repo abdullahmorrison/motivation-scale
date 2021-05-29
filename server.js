93862dfbb2bdd4e2b1bedcec746c46dc40e3a376
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-05-28 20:54:45 -0400

    fixed frontend dir

--- server.js
+++ server.js
@@ -24,10 +24,10 @@ app.use('/users', userRoutes);
 //server static assets if in production
 if(process.env.NODE_ENV === 'production'){
     //set static folder
-    app.use(express.static('../frontend/build'))
+    app.use(express.static('frontend/build'))
 
     app.get('*', (req, res)=>{
-        res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     })
 }
 
