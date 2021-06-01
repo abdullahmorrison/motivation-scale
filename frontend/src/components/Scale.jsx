cda71047c0e34315b8fa49220353545b4428eac4
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-06-01 09:53:22 -0400

    opened and closing explanation icon

--- frontend/src/components/Scale.jsx
+++ frontend/src/components/Scale.jsx
@@ -5,6 +5,7 @@ import WritingSpace  from "./WritingSpace";
 
 //SVG
 import ExplanationIconClosed from "./icons/ExplanationIconClosed";
+import ExplanationIconOpened from "./icons/ExplanationIconOpened";
 import DeleteIcon from "./icons/DeleteIcon";
 
 const Scale = ({scaleID, onDelete}) => {
@@ -18,8 +19,12 @@ const Scale = ({scaleID, onDelete}) => {
         <div className="scale">
             <div className="scale__header">
                 <ScaleTitle scaleID={scaleID}/>
-                <ExplanationIconClosed alt="Explanation Button (Closed)" onClick={handleWritingSpace}/>
-                <DeleteIcon alt="Delete Button" onClick={()=>onDelete(scaleID)}/> 
+                {
+                    writingSpaceVisible !== true 
+                        ? <ExplanationIconClosed alt="Explanation Button (Closed)" onClick={handleWritingSpace}/>
+                        : <ExplanationIconOpened alt="Explanation Button (Opened)" onClick={handleWritingSpace}/>
+                }
+               <DeleteIcon alt="Delete Button" onClick={()=>onDelete(scaleID)}/> 
             </div>
             <ScaleSlider scaleID={scaleID}/>
             {
