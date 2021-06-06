2dd41043f43765c0e942b9edb338cbb40b001cb4
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-06-05 21:44:21 -0400

    removed useEffect import

--- frontend/src/components/Scale.jsx
+++ frontend/src/components/Scale.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import ScaleTitle from './ScaleTitle';
 import ScaleSlider from './ScaleSlider';
 import WritingSpace  from "./WritingSpace";
