9d3348c4dfabfa0dfa5b7238d104c98c9b4bade6
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-08-25 15:42:37 -0400

    added routing

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,5 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var react_dom_1 = __importDefault(require("react-dom"));
-var PGPScale_1 = __importDefault(require("./components/PGPScale"));
-react_dom_1.default.render(<PGPScale_1.default />, document.getElementById('root'));
+var Landing_1 = __importDefault(require("./pages/Landing/Landing"));
+var PGPScale_1 = __importDefault(require("./pages/PGPScale/PGPScale"));
+var react_router_dom_1 = require("react-router-dom");
+react_dom_1.default.render(<react_router_dom_1.BrowserRouter>
+        <react_router_dom_1.Route path="/" exact component={Landing_1.default}/>
+        <react_router_dom_1.Route path="/pgpscale" component={PGPScale_1.default}/>
+    </react_router_dom_1.BrowserRouter>, document.getElementById('root'));
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,11 @@
 import ReactDOM from 'react-dom';
-import PGPScale from './components/PGPScale';
+import Landing from './pages/Landing/Landing';
+import PGPScale from './pages/PGPScale/PGPScale';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
-ReactDOM.render(<PGPScale />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <Route path="/" exact component={Landing}/>
+        <Route path="/pgpscale" component={PGPScale}/>
+    </Router>, 
+document.getElementById('root'));
