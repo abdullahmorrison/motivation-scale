ac07f11bc7ac6c553a2cea55c24ba86ed4dd31d0
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-08-26 16:22:34 -0400

    landing page skeleton

--- frontend/src/pages/Landing/Landing.jsx
+++ frontend/src/pages/Landing/Landing.jsx
@@ -1,6 +1,36 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+var GitHubLogo_1 = __importDefault(require("./icons/GitHubLogo"));
+var MailLogo_1 = __importDefault(require("./icons/MailLogo"));
 var Landing = function () {
-    return <div>EMPTY</div>;
+    return (<>
+        <header>
+            <h1>Prevent Your Emotions From Slowing Your Progress</h1>
+            <button><h2>Get Started</h2></button>
+        </header>
+        <div>
+            Demo
+        </div>
+        <div>
+            <a href="/"><h2>View an In-Depth Guide on This Tool &rarr;</h2></a>
+        </div>  
+        <footer>
+            <div>
+                <a href="mailto:abdullahmorrison@gmail.com" target="_blank" rel="noreferrer">
+                    Contact Me
+                    <MailLogo_1.default />
+                </a>
+            </div>
+            <div>
+                <a href="https://github.com/abdullahmorrison/ThePerceivedGoalProgressScale" target="_blank" rel="noreferrer">
+                    View My GitHub
+                    <GitHubLogo_1.default />
+                </a>
+            </div>
+        </footer>
+    </>);
 };
 exports.default = Landing;
--- frontend/src/pages/Landing/Landing.tsx
+++ frontend/src/pages/Landing/Landing.tsx
@@ -1,4 +1,34 @@
+import GitHubLogo from './icons/GitHubLogo'
+import MailLogo from './icons/MailLogo'
+
 const Landing = () => {
-    return <div>EMPTY</div>
+    return (
+    <>
+        <header>
+            <h1>Prevent Your Emotions From Slowing Your Progress</h1>
+            <button><h2>Get Started</h2></button>
+        </header>
+        <div>
+            Demo
+        </div>
+        <div>
+            <a href="/"><h2>View an In-Depth Guide on This Tool &rarr;</h2></a>
+        </div>  
+        <footer>
+            <div>
+                <a href="mailto:abdullahmorrison@gmail.com" target="_blank" rel="noreferrer">
+                    Contact Me
+                    <MailLogo/>
+                </a>
+            </div>
+            <div>
+                <a href="https://github.com/abdullahmorrison/ThePerceivedGoalProgressScale" target="_blank" rel="noreferrer">
+                    View My GitHub
+                    <GitHubLogo/>
+                </a>
+            </div>
+        </footer>
+    </>
+    )
 }
 export default Landing
