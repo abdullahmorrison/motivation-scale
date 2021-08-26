95453d0b3bb5513bf0bae2cb37d1a29ba2183326
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-08-26 18:01:05 -0400

    landing page skeleton without the demo section

--- frontend/src/pages/Landing/Landing.jsx
+++ frontend/src/pages/Landing/Landing.jsx
@@ -3,13 +3,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var GitHubLogo_1 = __importDefault(require("./icons/GitHubLogo"));
-var MailLogo_1 = __importDefault(require("./icons/MailLogo"));
+var githubLogo_png_1 = __importDefault(require("./images/githubLogo.png"));
+var mailLogo_png_1 = __importDefault(require("./images/mailLogo.png"));
+var laptop_png_1 = __importDefault(require("./images/laptop.png"));
 var Landing = function () {
     return (<>
         <header>
             <h1>Prevent Your Emotions From Slowing Your Progress</h1>
             <button><h2>Get Started</h2></button>
+            <img src={laptop_png_1.default} alt="laptop"/>
         </header>
         <div>
             Demo
@@ -21,13 +23,13 @@ var Landing = function () {
             <div>
                 <a href="mailto:abdullahmorrison@gmail.com" target="_blank" rel="noreferrer">
                     Contact Me
-                    <MailLogo_1.default />
+                    <img src={mailLogo_png_1.default} alt="Mail Logo"/>
                 </a>
             </div>
             <div>
                 <a href="https://github.com/abdullahmorrison/ThePerceivedGoalProgressScale" target="_blank" rel="noreferrer">
                     View My GitHub
-                    <GitHubLogo_1.default />
+                    <img src={githubLogo_png_1.default} alt="GitHub Logo"/>
                 </a>
             </div>
         </footer>
--- frontend/src/pages/Landing/Landing.tsx
+++ frontend/src/pages/Landing/Landing.tsx
@@ -1,5 +1,6 @@
-import GitHubLogo from './icons/GitHubLogo'
-import MailLogo from './icons/MailLogo'
+import GitHubLogo from './images/githubLogo.png'
+import MailLogo from './images/mailLogo.png'
+import Laptop from './images/laptop.png'
 
 const Landing = () => {
     return (
@@ -7,6 +8,7 @@ const Landing = () => {
         <header>
             <h1>Prevent Your Emotions From Slowing Your Progress</h1>
             <button><h2>Get Started</h2></button>
+            <img src={Laptop} alt="laptop"/>
         </header>
         <div>
             Demo
@@ -18,13 +20,13 @@ const Landing = () => {
             <div>
                 <a href="mailto:abdullahmorrison@gmail.com" target="_blank" rel="noreferrer">
                     Contact Me
-                    <MailLogo/>
+                    <img src={MailLogo} alt="Mail Logo" />
                 </a>
             </div>
             <div>
                 <a href="https://github.com/abdullahmorrison/ThePerceivedGoalProgressScale" target="_blank" rel="noreferrer">
                     View My GitHub
-                    <GitHubLogo/>
+                    <img src={GitHubLogo} alt="GitHub Logo" />
                 </a>
             </div>
         </footer>
