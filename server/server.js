0ca59b749ac6f41cfcfd4eedf616188e89b3b4a4
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-09-29 18:16:58 -0400

    route test

--- server/server.js
+++ server/server.js
@@ -14,22 +14,13 @@ app.use(cors({origin: 'http://localhost:3000'}))
 
 require('dotenv/config')//security (dotenv)
 
-
 const scaleRoutes = require('./routes/scales.js');
+app.use('https://pgpscale.netlify.app/pgpscale/scales', scaleRoutes);
 app.use('/scales', scaleRoutes);
 
 const userRoutes = require('./routes/users.js');
+app.use('https://pgpscale.netlify.app/pgpscale/users', userRoutes);
 app.use('/users', userRoutes);
-
-//server static assets if in production
-if(process.env.NODE_ENV === 'production'){
-    //set static folder
-    app.use(express.static('frontend/build'))
-
-    app.get('*', (req, res)=>{
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    })
-}
 
 const port = process.env.PORT || 3001;
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true})
