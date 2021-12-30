daebf3e802c008b1279a4f7a0f921c2875228efc
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-12-29 19:29:54 -0500

    renamed file

--- client/src/pages/Landing/components/Hero/HeroScale.tsx
+++ client/src/pages/Landing/components/Hero/HeroScale.tsx
@@ -1,7 +1,8 @@
 const ScaleAnimation = () =>{
     return(
-        <>
-        </>
+        <div>
+            <input type="range" name="scale"/>
+        </div>
     )
 }
 export default ScaleAnimation
