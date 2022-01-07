c41b77e3e8aa3237515f32e7547458fc88b3b3f6
Abdullah Morrison <abdullahmorrison@gmail.com>
2022-01-06 19:10:40 -0500

    landing: basic pgp explanaiton set up

--- client/src/pages/Landing/components/PGPExplanation.tsx
+++ client/src/pages/Landing/components/PGPExplanation.tsx
@@ -1,6 +1,18 @@
+import styles from './pgpExplanation.module.css'
+
 const PGPExplanation = () => {
     return (
-        <></>
+        <div className={styles.explanation}>
+            <section>
+                <h2>
+                    This tool helps create motivation and keep it positive
+                </h2>
+                <p>
+                    It does this by improving your self-awareness and helping you plan your goals
+                </p>
+            </section>
+            <img src="" alt="Motivation Source on a Man" />
+        </div>
     )
 }
 export default PGPExplanation
