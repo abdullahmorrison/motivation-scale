5255b2b768b8be70ce46827a4bdbb75888665c03
Abdullah Morrison <abdullahmorrison@gmail.com>
2022-01-09 08:00:58 -0500

    landing: nav added

--- client/src/pages/Landing/components/Nav.tsx
+++ client/src/pages/Landing/components/Nav.tsx
@@ -1,7 +1,13 @@
+import styles from './nav.module.css'
+
 const Nav = () =>{
     return(
-        <>
-        </>
+        <nav className={styles.nav}>
+            <ul>
+                <li><a href="/" className={styles.logo}>PGP Scale</a></li>
+                <li><a href="" className={styles.unstyled}>Login</a> | < a href="">Sign Up &rarr;</a></li>
+            </ul>
+        </nav>
     )
 }
 export default Nav
