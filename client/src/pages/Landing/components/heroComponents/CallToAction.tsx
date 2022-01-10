e9905fa27692cfe71d1f48a113dc1d9aef6fd91a
Abdullah Morrison <abdullahmorrison@gmail.com>
2022-01-10 07:57:46 -0500

    landing: google and apple store css

--- client/src/pages/Landing/components/heroComponents/CallToAction.tsx
+++ client/src/pages/Landing/components/heroComponents/CallToAction.tsx
@@ -9,7 +9,7 @@ const CallToAction = () =>{
             <h1>The Perceived Goal Progress Scale</h1>
             <h2>Prevent Your Emotions From Slowing Your Progress</h2>
             <button>Get Started</button>
-            <div className="download-app">
+            <div className={styles.downloadApp}>
                 <a href=''><img src={appleStoreButton} alt="Apple Store Button" /></a>
                 <a href=''><img src={googlePlayButton} alt="Google Play Button" /></a>
             </div>
