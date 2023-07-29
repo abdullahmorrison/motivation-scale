8aa165ae3bde264915de9ef8f59a884a7926362d
abdullahmorrison <abdullahmorrison@gmail.com>
2023-07-29 18:02:48 -0400

    graphql server set up (#14)

---
+++ server/src/index.ts
@@ -0,0 +1,9 @@
+import { ApolloServer } from "apollo-server"
+
+import { schema } from "./schema"
+export const server = new ApolloServer({ schema })
+
+const port = process.env.PORT || 4000
+server.listen({ port }).then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`)
+})
