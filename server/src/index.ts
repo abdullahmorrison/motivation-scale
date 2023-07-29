ffa35cfc11df603134733bd52869ef52e30daf03
abdullahmorrison <abdullahmorrison@gmail.com>
2023-07-29 18:52:25 -0400

    allow running apollo offline (#14)

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,11 @@
 import { ApolloServer } from "apollo-server"
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core"
 
 import { schema } from "./schema"
-export const server = new ApolloServer({ schema })
+export const server = new ApolloServer({ 
+    schema,
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+})
 
 const port = process.env.PORT || 4000
 server.listen({ port }).then(({ url }) => {
