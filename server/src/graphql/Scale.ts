4e748dcab41921e613d415d7e30e439da134043c
abdullahmorrison <abdullahmorrison@gmail.com>
2023-07-30 18:56:08 -0400

    graphql scale get query (#14)

--- server/src/graphql/Scale.ts
+++ server/src/graphql/Scale.ts
@@ -11,3 +11,32 @@ export const Scale = objectType({
         t.nullable.string("avoidingFailureDescription");
     },
 })
+
+let scales:  NexusGenObjects["Scale"][] = [
+    {
+        id: "1",
+        goal: "I want to be a better person",
+        sliderValue: 0,
+        chasingSuccessDescription: "I want to be a better person",
+        avoidingFailureDescription: "I don't want to be a bad person",
+    },
+    {
+        id: "2",
+        goal: "I want to be a better person",
+        sliderValue: 0,
+        chasingSuccessDescription: "I want to be a better person",
+        avoidingFailureDescription: "I don't want to be a bad person",
+    }
+]
+
+export const ScaleQuery = extendType({
+    type: "Query",
+    definition(t) {
+        t.nonNull.list.nonNull.field("scales", {
+            type: "Scale",
+            resolve(parent, args, context, info) {
+                return scales;
+            }
+        });
+    }
+})
