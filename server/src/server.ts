26ab2b6be790898846d8350792478333c46fedab
abdullahmorrison <abdullahmorrison@gmail.com>
2023-08-01 20:54:55 -0400

    formatting

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,6 @@ import { ApolloServer } from 'apollo-server';
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core"
 import express from 'express'
 import { connect, set } from 'mongoose'
-// import { createHandler } from 'graphql-http/lib/use/express'
 import { schema } from './schema'
 import bodyParser from 'body-parser'
 
@@ -11,7 +10,6 @@ const app = express();
 app.use(bodyParser.json({ limit: '30mb'}))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
-//cors = cross-origin resource sharing
 const cors = require('cors');
 var whitelist = ['http://localhost:3000', 'https://pgpscale.netlify.app']
 app.use(cors({
@@ -32,7 +30,6 @@ const server = new ApolloServer({
     express: app,
     plugins: [ApolloServerPluginLandingPageGraphQLPlayground()]
 } as any)
-
 
 const port = process.env.PORT || 3001;
 connect(process.env.DB_CONNECTION as string, { useNewUrlParser: true, useUnifiedTopology: true, dbName: 'myFirstDatabase'})
