c3d2ad6b20968c2ef9c205aec8d04c2f3e22d44b
abdullahmorrison <abdullahmorrison@gmail.com>
2023-10-04 17:44:54 -0400

    added apollo/client to website (#26)

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -2,16 +2,24 @@ import ReactDOM from 'react-dom';
 import Landing from './pages/Landing/Landing';
 import PGPScale from './pages/PGPScale/PGPScale';
 import NotFound from './pages/404Page/NotFound';
-import InDevelopmentBanner from './pages/InDevelopmentBanner/InDevelopmentBanner'
 import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 
+const httpLink = createHttpLink({
+    uri: "https://motivationscale.up.railway.app",
+})
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 ReactDOM.render(
-    <Router>
-        <InDevelopmentBanner/>
-        <Switch>
-            <Route exact path="/" component={Landing}/>
-            <Route exact path="/pgpscale" component={PGPScale}/>
-            <Route exact path="*" component={NotFound} />
-        </Switch>
-    </Router>, 
+    <ApolloProvider client={client}>
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Landing}/>
+                <Route exact path="/pgpscale" component={PGPScale}/>
+                <Route exact path="*" component={NotFound} />
+            </Switch>
+        </Router>
+    </ApolloProvider>, 
 document.getElementById('root'));
