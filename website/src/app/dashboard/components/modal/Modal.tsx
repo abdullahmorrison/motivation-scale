98885622c9a860a7f0c7a048f391a5e507282d29
abdullahmorrison <abdullahmorrison@gmail.com>
2023-10-07 13:56:23 -0400

    changed confirm modal to abstract modal

---
+++ website/src/app/dashboard/components/modal/Modal.tsx
@@ -0,0 +1,36 @@
+import styles from './modal.module.scss'
+
+interface ModalProps{
+    title: string
+    message: string,
+    isVisible: boolean,
+    buttons: {
+        text: string,
+        backgroundColor?: string,
+        onClick?: () => void
+    }[]
+}
+const ConfirmModal = (props: ModalProps) =>{
+    return (
+        <div className={styles.modal} style={props.isVisible? undefined : {visibility: "hidden"}}>
+            <div className={styles.modalContent}>
+                <div className={styles.header}><h3>{props.title}</h3></div>
+                <div>
+                    {props.message}
+                </div>
+                <footer>
+                    {props.buttons.map((button, index) => (
+                        <button 
+                            key={index} 
+                            style={{backgroundColor: button.backgroundColor}} 
+                            onClick={button.onClick}
+                        >
+                            {button.text}
+                        </button>
+                    ))}
+                </footer>
+            </div>
+        </div>
+    )
+}
+export default ConfirmModal
