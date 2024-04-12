97d1f17947bdb91b0eccd6f72f20d8477645e059
Abdullah Morrison <abdullahmorrison@gmail.com>
2024-04-12 19:11:35 -0400

    updated user schema

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -3,11 +3,18 @@ import { Schema, model } from 'mongoose';
 const userSchema = new Schema({
   email: {
     type: String,
+    required: true,
     unique: true,
     trim: true
   },
-  password: type: String,
-  token: String
+  password: {
+    type: String,
+    required: true
+  },
+  token: {
+    type: String,
+    required: true
+  }
 }, {
   timestamps: true,
 });
