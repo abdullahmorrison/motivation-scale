beef6f9b9147624ac7c32d839f28cd477d498696
Abdullah Morrison <abdullahmorrison@gmail.com>
2024-04-21 12:07:34 -0400

    tests for rejecting registering user that already exists

--- server/src/tests/index.test.ts
+++ server/src/tests/index.test.ts
@@ -1,6 +1,7 @@
 import { ApolloServer } from 'apollo-server';
 import { disconnect } from 'mongoose';
 import { schema } from "../schema"
+import { UserModel } from "../models/user"
 import { app } from "../server"
 import { REGISTER_USER, LOGIN_USER } from "./queries/user"
 import {ERROR_LIST} from '../utils/error-handler.helper';
@@ -9,19 +10,28 @@ import {ERROR_LIST} from '../utils/error-handler.helper';
 describe("Login/Register", ()=>{
   let testServer: ApolloServer
 
+  const email = "test@gmail.com"
+  const password = "test"
+
   beforeAll(async ()=>{
     testServer = new ApolloServer({
       schema,
       express: app
     } as any)
+
+    // make sure we are only making queries to the test database
+    const env = process.env.DB_NAME
+    if(env===undefined || env!=="test")
+      throw new Error(`environment variable (${env}) not set to test`)
+
+    // make sure the test user does not already exits (already registered)
+    const response = await UserModel.find({email}).catch()
+    if(response) await UserModel.findOneAndDelete(response.id) //remove from db if exists
   })
   afterAll(async ()=>{
     await testServer.stop()
     await disconnect()
   })
-
-  const email = "test@gmail.com"
-  const password = "test"
 
   it("Registers a new user", async ()=>{
     const response = await testServer.executeOperation({
@@ -31,8 +41,16 @@ describe("Login/Register", ()=>{
     expect(response.errors).toBe(undefined)
     expect(response.data?.registerUser.email).toBe(email)
   })
-  //missing: Register: reject user already exists
-  
+  it("Register: reject already existing user email", async ()=>{
+    const alreadyExisitingUserEmail = "existingemail@gmail.com"
+    const alreadyExistingUserPassword = "test"
+
+    const response = await testServer.executeOperation({
+      query: REGISTER_USER,
+      variables: { email: alreadyExisitingUserEmail, password: alreadyExistingUserPassword}
+    })
+    expect(response.errors?.at(0)?.extensions?.code).toBe(ERROR_LIST.ALREADY_EXISTS.code)
+  })
   it("Register: reject invalid email", async ()=>{
     const invalidEmails = ["invalidemail", "invalidemail@2", "invalidemail.com", "invalid@email."]
 
