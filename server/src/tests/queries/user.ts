56b38159f1d8f136cca1a46170cfd3576a6d5544
Abdullah Morrison <abdullahmorrison@gmail.com>
2024-04-23 20:06:50 -0400

    merged test user queries to one object

--- server/src/tests/queries/user.ts
+++ server/src/tests/queries/user.ts
@@ -1,27 +1,28 @@
-export const REGISTER_USER = `
-  mutation registerUser(
-    $email: String!
-    $password: String!
-  ) {
-    registerUser(
-      email: $email
-      password: $password
+export const userQueries = {
+  REGISTER_USER: `
+    mutation registerUser(
+      $email: String!
+      $password: String!
     ) {
-      id
-      email
-      password
-      token
+      registerUser(
+        email: $email
+        password: $password
+      ) {
+        id
+        email
+        password
+        token
+      }
     }
-  }
-`
-
-export const LOGIN_USER = `
-  mutation loginUser($email: String!, $password: String!){
-    loginUser(email: $email, password: $password){
-      id
-      email
-      password
-      token
+  `,
+  LOGIN_USER: `
+    mutation loginUser($email: String!, $password: String!){
+      loginUser(email: $email, password: $password){
+        id
+        email
+        password
+        token
+      }
     }
-  }
-`
+  `
+}
