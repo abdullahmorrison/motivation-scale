c0c4e5b969d456f7c6411b3acd553ece0f5762cc
Abdullah Morrison <abdullahmorrison@gmail.com>
2024-04-23 20:01:04 -0400

    merged test scale queries to one object

--- server/src/tests/queries/scale.ts
+++ server/src/tests/queries/scale.ts
@@ -1,62 +1,63 @@
-export const GET_SCALES=`
-  query GetScales($userId: String!) {
-    scales(userId: $userId) {
-      id
-      userId
-      goal
-      sliderValue
-      chasingSuccessDescription
-      avoidingFailureDescription
+export const ScaleQueries = {
+  GET_SCALES:`
+    query GetScales($userId: String!) {
+      scales(userId: $userId) {
+        id
+        userId
+        goal
+        sliderValue
+        chasingSuccessDescription
+        avoidingFailureDescription
+      }
     }
-  }
-`
-export const CREATE_SCALE=`
-  mutation CreateScale(
-    $userId: String!
-    $goal: String!
-    $sliderValue: Int
-    $chasingSuccessDescription: String
-    $avoidingFailureDescription: String
-  ) {
-    createScale(
-      userId: $userId
-      goal: $goal
-      sliderValue: $sliderValue
-      chasingSuccessDescription: $chasingSuccessDescription
-      avoidingFailureDescription: $avoidingFailureDescription
+  `,
+  CREATE_SCALE:`
+    mutation CreateScale(
+      $userId: String!
+      $goal: String!
+      $sliderValue: Int
+      $chasingSuccessDescription: String
+      $avoidingFailureDescription: String
     ) {
-      id
-      userId
-      goal
-      sliderValue
-      chasingSuccessDescription
-      avoidingFailureDescription
+      createScale(
+        userId: $userId
+        goal: $goal
+        sliderValue: $sliderValue
+        chasingSuccessDescription: $chasingSuccessDescription
+        avoidingFailureDescription: $avoidingFailureDescription
+      ) {
+        id
+        userId
+        goal
+        sliderValue
+        chasingSuccessDescription
+        avoidingFailureDescription
+      }
     }
-  }
-`
-export const UPDATE_SCALE=`
-  mutation UpdateScale(
-    $id: String!
-    $userId: String!
-    $goal: String
-    $sliderValue: Int
-    $chasingSuccessDescription: String
-    $avoidingFailureDescription: String
-  ) {
-    updateScale(
-      id: $id
-      userId: $userId
-      goal: $goal
-      sliderValue: $sliderValue
-      chasingSuccessDescription: $chasingSuccessDescription
-      avoidingFailureDescription: $avoidingFailureDescription
+  `,
+  UPDATE_SCALE:`
+    mutation UpdateScale(
+      $id: String!
+      $userId: String!
+      $goal: String
+      $sliderValue: Int
+      $chasingSuccessDescription: String
+      $avoidingFailureDescription: String
     ) {
-      id
-      goal
-      sliderValue
-      chasingSuccessDescription
-      avoidingFailureDescription
+      updateScale(
+        id: $id
+        userId: $userId
+        goal: $goal
+        sliderValue: $sliderValue
+        chasingSuccessDescription: $chasingSuccessDescription
+        avoidingFailureDescription: $avoidingFailureDescription
+      ) {
+        id
+        goal
+        sliderValue
+        chasingSuccessDescription
+        avoidingFailureDescription
+      }
     }
-  }
-`
-export const DELETE_SCALE=``
+  `
+}
