644dafb419b33d89e5d69fd337d16b9eddd71dca
Abdullah Morrison <abdullahmorrison@gmail.com>
2024-05-06 16:54:58 -0400

    created form hook

---
+++ website/src/hooks/useForm.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+export default function useForm(callback: (...values: any)=>{}, initialState: any = {}){
+  const [values, setValues] = useState(initialState)
+
+  function onChange(event: any){
+    setValues((prevValues: any) => { return { ...prevValues, [event.target.name]: event.target.value}})
+  }
+  function onSubmit(event: any){
+    event.preventDefault()
+    callback()
+  }
+  return { onChange, onSubmit, values }
+}
