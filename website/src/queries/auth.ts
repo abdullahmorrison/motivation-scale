b3d4b14ee14f606b10c1d83aceb166545d749e26
Abdullah Morrison <abdullahmorrison@gmail.com>
2024-05-06 17:01:22 -0400

    created queries folder with auth queries

---
+++ website/src/queries/auth.ts
@@ -0,0 +1,12 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!){
+    loginUser(email: $email, password: $password){
+      id
+      email
+      password
+      token
+    }
+  }
+`
