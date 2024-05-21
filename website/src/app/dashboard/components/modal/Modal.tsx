774a5761446fb1fb286c8976e2e5342c4ef29acf
Abdullah Morrison <abdullahmorrison@gmail.com>
2024-05-21 15:28:01 -0400

    modal overhaul, made it exclusive to a scale

--- website/src/app/dashboard/components/modal/Modal.tsx
+++ website/src/app/dashboard/components/modal/Modal.tsx
@@ -1,36 +1,72 @@
+import { PropsWithChildren, useState } from 'react'
 import styles from './modal.module.scss'
 
-interface ModalProps{
-    title: string
-    message: string,
-    isVisible: boolean,
-    buttons: {
-        text: string,
-        backgroundColor?: string,
-        onClick?: () => void
-    }[]
+type ScaleModalProps = {
+  type: "add" | "edit" | null,
+  scale?: {
+    id?: string,
+    goal?: string,
+    chasingSuccessDescription?: string,
+    avoidingFailureDescription?: string
+  }
+  onEdit: (scaleData: any)=> void
+  onAdd: (scaleData: any)=> void
+  onDelete: (scaleData: any)=> void
+  onClose: ()=>void
 }
-const ConfirmModal = (props: ModalProps) =>{
-    return (
-        <div className={styles.modal} style={props.isVisible? undefined : {visibility: "hidden"}}>
-            <div className={styles.modalContent}>
-                <div className={styles.header}><h3>{props.title}</h3></div>
-                <div>
-                    {props.message}
-                </div>
-                <footer>
-                    {props.buttons.map((button, index) => (
-                        <button 
-                            key={index} 
-                            style={{backgroundColor: button.backgroundColor}} 
-                            onClick={button.onClick}
-                        >
-                            {button.text}
-                        </button>
-                    ))}
-                </footer>
-            </div>
+const ScaleModal = (props: PropsWithChildren<ScaleModalProps>) =>{
+  const [modalData, setModalData] = useState(props.scale)
+
+  return (
+    <div className={styles.modal}>
+      <div className={styles.modalContent}>
+        <span className={styles.closeModal} onClick={props.onClose}>&times;</span>
+        <div className={styles.header}>
+          <h3>
+            {props.type=='add' ? "Create a new Scale"
+            : props.type=="edit" ? "Edit Scale" : null}
+          </h3>
         </div>
-    )
+        <label htmlFor="goal">Goal</label>
+        <input
+          type="text"
+          name="goal"
+          placeholder="Enter you goal..."
+          onChange={(e)=>setModalData({...modalData, goal: e.target.value})}
+          defaultValue={modalData?.goal} required={true}
+        />
+
+        <label htmlFor="success-metrics">Metrics for Chasing Success</label>
+        <textarea
+          name="success-metrics"
+          placeholder='Your metrics for chasing success...'
+          onChange={(e)=>setModalData({...modalData, chasingSuccessDescription: e.target.value})}
+          defaultValue={modalData?.chasingSuccessDescription}
+        />
+
+        <label htmlFor="failure-metrics">Metrics for Avoiding Failure</label>
+        <textarea
+          name="failure-metrics"
+          placeholder='Your metrics for avoiding failure...'
+          onChange={(e)=>setModalData({...modalData, avoidingFailureDescription: e.target.value})}
+          defaultValue={modalData?.avoidingFailureDescription}
+        />
+
+        <div className={styles.buttons}>
+          {props.type=="edit" ?
+            <>
+            <button className="danger" onClick={props.onDelete}>Delete Scale</button>
+            <button className="commit" onClick={()=> props.onEdit(modalData)}>Update Scale</button>
+            </>
+          :props.type=="add" ?
+            <>
+            <button onClick={props.onClose}>Cancel</button>
+            <button className="commit" onClick={()=> props.onAdd(modalData)}>Create Scale</button>
+            </>
+          : null}
+        </div>
+      </div>
+    </div>
+  )
 }
-export default ConfirmModal
+export default ScaleModal
