8b31587019a5de24226d33ce8d4fca01ad4dbccf
Abdullah Morrison <abdullahmorrison@gmail.com>
2024-05-21 17:47:16 -0400

    created debounced useState hook

---
+++ website/src/hooks/useStateDebounced.tsx
@@ -0,0 +1,40 @@
+//Code from: https://stackoverflow.com/questions/64579144/how-to-add-debounce-to-react-onchange-input
+import { useMemo, useState } from 'react'
+
+const debounce = (fn: any, delay: number) => {
+  let timeout: NodeJS.Timeout | null = null
+  return (...args: any[]) => {
+    if (timeout) clearTimeout(timeout)
+    timeout = setTimeout(fn, delay, ...args)
+  }
+}
+
+export default function useStateDebounced<T>(initialValue: T, delay: number, callback: ()=>any) {
+  const [inputValue, _setInputValue] = useState<T>(initialValue)
+
+  const [debouncedInputValue, setDebouncedInputValue] = useState<T>(initialValue)
+
+  const memoizedDebounce = useMemo(
+    () =>
+      debounce((value: T) => {
+        setDebouncedInputValue(value)
+        callback()
+      }, delay),
+    [delay]
+  )
+
+  const setInputValue = (value: T | ((prevState: T) => T)) => {
+    if (value instanceof Function) {
+      _setInputValue((p) => {
+        const mutated = value(p)
+        memoizedDebounce(mutated)
+        return mutated
+      })
+    } else {
+      _setInputValue(value)
+      memoizedDebounce(value)
+    }
+  }
+
+  return [inputValue, debouncedInputValue, setInputValue] as const
+}
