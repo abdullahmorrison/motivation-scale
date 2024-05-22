20629751cc92f30f5478324892d351d3ca249864
Abdullah Morrison <abdullahmorrison@gmail.com>
2024-05-22 12:17:12 -0400

    updated scale metrics section

--- website/src/app/dashboard/components/scale/Scale.tsx
+++ website/src/app/dashboard/components/scale/Scale.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
-
+import parse from 'html-react-parser';
+  
 //Components
 import ScaleSlider from '../scale-slider/ScaleSlider';
-import WritingSpace  from "../writing-space/WritingSpace";
 
 //third party libraries
 import { Draggable, DraggableProvided, DraggableStateSnapshot } from 'react-beautiful-dnd';
@@ -23,7 +23,12 @@ export interface ScaleType{
     onEdit: (scaleID: string) => void 
 }
 const Scale = (props: ScaleType) => {
-    const [writingSpaceVisible, setWritiingSpaceVisible] = useState<boolean>(false)
+    const [isMetricsVisible, setMetricsVisible] = useState<boolean>(false)
+
+    function renderText(htmlString: string | undefined){
+      if(!htmlString) return ""
+      return parse(""+htmlString.replaceAll("\n", " <br/> "))
+    }
 
     return (
         <Draggable key={props.id} draggableId={"draggable-"+props.id} index={props.index}>
@@ -41,13 +46,19 @@ const Scale = (props: ScaleType) => {
                         </div>
                     </div>
                     <ScaleSlider id={props.id} sliderValue={props.sliderValue}/>
-                    <WritingSpace 
-                        id={props.id} 
-                        isVisible={writingSpaceVisible}
-                        avoidingFailureDescription={props.avoidingFailureDescription}
-                        chasingSuccessDescription={props.chasingSuccessDescription}
-                    />
-                    <div className={styles.dropdown} onClick={()=>setWritiingSpaceVisible(!writingSpaceVisible)}>{writingSpaceVisible ? "▲" : "▼"}</div>
+
+                    <div className={styles.metrics} style={isMetricsVisible? {display:"flex"} : {display:"none"}}>
+                        <div>
+                            <h3>Chasing Success Metrics</h3>
+                            <p>{renderText(props.chasingSuccessDescription)}</p>
+                        </div>
+                        <div>
+                            <h3>Avoiding Failure Metrics</h3>
+                            <p>{renderText(props.avoidingFailureDescription)}</p>
+                        </div>
+                    </div>
+
+                    <div className={styles.dropdown} onClick={()=>setMetricsVisible(!isMetricsVisible)}>{isMetricsVisible ? "▲" : "▼"}</div>
                 </div>
             )}
          </Draggable>
