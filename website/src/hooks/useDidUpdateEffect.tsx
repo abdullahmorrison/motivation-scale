afdb73fbe26f17a983b94aa8597f1a7570f9dcd0
Abdullah Morrison <abdullahmorrison@gmail.com>
2024-05-22 01:13:44 -0400

    added useDidUpdateEffect hook

---
+++ website/src/hooks/useDidUpdateEffect.tsx
@@ -0,0 +1,18 @@
+//Took from: https://stackoverflow.com/questions/53179075/with-useeffect-how-can-i-skip-applying-an-effect-upon-the-initial-render
+import { useEffect, useRef } from "react";
+
+export default function useDidUpdateEffect(fn: ()=>any, inputs: any) {
+  const isMountingRef = useRef(false);
+
+  useEffect(() => {
+    isMountingRef.current = true;
+  }, []);
+
+  useEffect(() => {
+    if (!isMountingRef.current) {
+      return fn();
+    } else {
+      isMountingRef.current = false;
+    }
+  }, inputs);
+}
