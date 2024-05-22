d087785ad1542c0ac244edd8391aa973916d10ff
Abdullah Morrison <abdullahmorrison@gmail.com>
2024-05-22 09:51:14 -0400

    created account settings component

---
+++ website/src/app/settings/Settings.tsx
@@ -0,0 +1,23 @@
+import { useContext, useState } from 'react'
+import styles from './settings.module.scss'
+import { AuthContext } from '@/context/authContext'
+import Logo from "../assets/icons/logo.svg"
+
+const Settings = () => {
+  const { user }= useContext(AuthContext)
+  const [toggleSettingsOpen, setToggleSettingsOpen] = useState(false)
+
+  return (
+    <div className={styles.account}>
+      <div className={styles.icon} onClick={()=>setToggleSettingsOpen(prev=>!prev)}>{user.email.charAt(0).toUpperCase()}</div>
+      {toggleSettingsOpen ?
+        <div className={styles.settings}>
+          <h3>Account</h3>
+          <p className={styles.email}>{user.email}</p>
+          <p className={styles.logout}>Logout</p>
+        </div>
+      : null}
+    </div>
+  )
+}
+export default Settings
