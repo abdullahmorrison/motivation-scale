c75a72875dbfc9ad3899cb74186a8dfd81c031be
Abdullah Morrison <abdullahmorrison@gmail.com>
2024-06-03 17:34:28 -0400

    created style variables

---
+++ mobile/styles.variables.ts
@@ -0,0 +1,16 @@
+const variables = {
+  background: "#010101",
+  primary: "#212121",
+  secondary: "#FFFFFF",
+  highlight: "#4C4C4C",
+
+  boxShadow: "0 2px 4px #C2C2C250",
+  boxShadowClicked: "0 0 4px #C2C2C250",
+
+  textPrimary: "#FFFFFF",
+  textLink: "#2491E8",
+
+  buttonDanger: "#A80000",
+  buttonCommit: "#148402"
+} as const
+export default variables
