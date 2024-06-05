98fb88ed3e8a5a22ca680a3ed8f886f729768732
Abdullah Morrison <abdullahmorrison@gmail.com>
2024-06-05 15:46:53 -0400

    set up base navigation

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,12 @@
-import { AppRegistry } from "react-native"
 import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from "@apollo/client"
 import RootComponent from "./RootComponent"
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import LoginScreen from './Auth/Login';
+import SignupScreen from './Auth/Signup';
+import SplashScreen from "./SplashScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
     const httpLink = createHttpLink({
@@ -12,8 +18,15 @@ export default function App() {
     })
 
     return (
+      <NavigationContainer>
         <ApolloProvider client={client}>
-            <RootComponent />
+          <Stack.Navigator>
+            <Stack.Screen name="Splash Screen" component={SplashScreen} />
+            <Stack.Screen name="Signup" component={SignupScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Dashboard" component={RootComponent} />
+          </Stack.Navigator>
         </ApolloProvider>
+      </NavigationContainer>
     )
 }
