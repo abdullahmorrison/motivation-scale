ffec233a2617b43c6a1e188eed02f74e39445c07
Abdullah Morrison <abdullahmorrison@gmail.com>
2024-06-10 08:12:02 -0400

    scale reorder server functionality

---
+++ server/src/graphql/scaleOrder.ts
@@ -0,0 +1,40 @@
+import { objectType, extendType, nonNull, stringArg, list } from "nexus"
+import { ScaleModel } from "../models/scale";
+import { ScaleOrderModel } from "../models/scaleOrder";
+import { UserModel } from "../models/user";
+import throwCustomError, { ERROR_LIST } from '../utils/error-handler.helper'
+
+export const ScaleOrder = objectType({
+    name: "ScaleOrder",
+    definition(t) {
+        t.nonNull.string("userId");
+        t.nullable.list.string("scaleOrder")
+    },
+})
+
+export const reorderScales = extendType({
+    type: "Mutation",
+    definition(t) {
+        t.nonNull.field("reorderScales", {
+            type: "ScaleOrder",
+            description: "Reorder a scale",
+            args: {
+                userId: nonNull(stringArg()),
+                scaleOrder: nonNull(list(stringArg()))
+            },
+            resolve: async (_, args) => {
+                await UserModel.findById(args.userId) //check user exists
+                  .catch(()=> throwCustomError(ERROR_LIST.NOT_FOUND, "User with that id does not exist"))
+                const scales = await ScaleModel.find({userId: args.userId}) //check scale exists
+                  .catch(()=> throwCustomError(ERROR_LIST.NOT_FOUND, "Scales with that user id does not exist"))
+
+                for(let scale of scales){
+                  if(!args.scaleOrder.includes(scale.id))
+                    throwCustomError(ERROR_LIST.FORBIDDEN, "Unauthorized scale reorder")
+                }
+
+                return await ScaleOrderModel.findOneAndReplace({userId: args.userId}, {...args, scaleOrder: args.scaleOrder})
+            }
+        })
+    }
+})
