190a4e4465c7301ad61afcb4a1ea1db0accd34f8
Abdullah Morrison <abdullahmorrison@gmail.com>
2024-07-27 16:17:07 -0400

    mobile: added async storage for auth

--- mobile/Storage.ts
+++ mobile/Storage.ts
@@ -1,3 +1,4 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import Storage from 'react-native-storage';
 
 const storage = new Storage({
@@ -6,7 +7,7 @@ const storage = new Storage({
 
   // Use AsyncStorage for RN apps, or window.localStorage for web apps.
   // If storageBackend is not set, data will be lost after reload.
-  // storageBackend: AsyncStorage, // for web: window.localStorage
+  storageBackend: AsyncStorage, // for web: window.localStorage
 
   // expire time, default: 1 day (1000 * 3600 * 24 milliseconds).
   // can be null, which means never expire.
