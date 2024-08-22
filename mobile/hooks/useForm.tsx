3bb068de6b54eae81dd740088a8dcee0b875901a
Abdullah Morrison <abdullahmorrison@gmail.com>
2024-08-22 17:19:25 -0400

    mobile: useForm update, made callback function optional

--- mobile/hooks/useForm.tsx
+++ mobile/hooks/useForm.tsx
@@ -1,13 +1,13 @@
 import { useState } from "react"
 
-export default function useForm<T extends object>(callback: ()=>void, initialState: T = {} as T){
+export default function useForm<T extends object>(callback?: ()=>void, initialState: T = {} as T){
   const [values, setValues] = useState<T>(initialState)
 
   function onChange(key: keyof T, value: T[keyof T]){
     setValues((prevValues: T) => { return { ...prevValues, [key]: value}})
   }
   function onSubmit(){
-    callback()
+    if(callback) callback()
   }
   return { onChange, onSubmit, values }
 }
