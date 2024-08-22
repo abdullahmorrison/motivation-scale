53ebc46210704b33cb5d6653bfe43207cd47157f
Abdullah Morrison <abdullahmorrison@gmail.com>
2024-08-22 11:47:31 -0400

    added generics to useForm hook

--- mobile/hooks/useForm.tsx
+++ mobile/hooks/useForm.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react"
 
-export default function useForm(callback: (...values: any)=>{}, initialState: any = {}){
-  const [values, setValues] = useState(initialState)
+export default function useForm<T extends object>(callback: ()=>void, initialState: T = {} as T){
+  const [values, setValues] = useState<T>(initialState)
 
-  function onChange(key: any, value: any){
-    setValues((prevValues: any) => { return { ...prevValues, [key]: value}})
+  function onChange(key: keyof T, value: T[keyof T]){
+    setValues((prevValues: T) => { return { ...prevValues, [key]: value}})
   }
   function onSubmit(){
     callback()
