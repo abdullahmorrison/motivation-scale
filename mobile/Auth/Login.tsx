08912d4220a06ee5c8b52862f5bc3985efd42daf
Abdullah Morrison <abdullahmorrison@gmail.com>
2024-10-23 16:26:36 -0400

    mobile: added auth error pop ups

--- mobile/Auth/Login.tsx
+++ mobile/Auth/Login.tsx
@@ -1,16 +1,17 @@
-import { View, Dimensions, Button, Text, TextInput, StyleSheet, SafeAreaView } from "react-native"
+import { View, Dimensions, Button, Text, TextInput, StyleSheet, KeyboardAvoidingView, Platform, Keyboard, SafeAreaView } from "react-native"
 import { Link } from "@react-navigation/native" 
 import variables from "../styles.variables"
 import useForm from "../hooks/useForm"
 import { useMutation } from "@apollo/client"
 import { LOGIN_USER } from "../queries/auth"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { AuthContext } from "../context/authContext"
 import { screens } from "../screens"
 import { AuthInput, emptyAuthInput } from "../types/auth"
 
 export default function LoginScreen({ navigation }: {navigation: any}){
   const context = useContext(AuthContext)
+  const [loginError, setLoginError] = useState<string>()
 
   const { onChange, onSubmit, values } = useForm<AuthInput>(handleSubmit, emptyAuthInput)
 
@@ -23,25 +24,49 @@ export default function LoginScreen({ navigation }: {navigation: any}){
       context.login(loginUser)
       navigation.navigate(screens.Dashboard)
     },
-    onError(e){ console.log(e.message) }
+    onError(e){ setLoginError(e.message) }
   })
 
   return (
-    <SafeAreaView style={styles.contentContainer}>
-      <Text style={styles.h2}>Login to the Motivation Scale</Text>
-
-      <View style={styles.form}>
-        <Text style={styles.text}>Email</Text>
-        <TextInput style={styles.textInput} onChangeText={email=>onChange("email", email)} placeholder="Enter you email" placeholderTextColor={variables.highlight}/>
-
-        <Text style={styles.text}>Password</Text>
-        <TextInput secureTextEntry={true} style={styles.textInput} onChangeText={password=>onChange("password", password)} placeholder="Enter you password" placeholderTextColor={variables.highlight}/>
-
-        <Button color={variables.highlight} title="Login" onPress={onSubmit}/>
+  <SafeAreaView style={styles.contentContainer}>
+    {loginError &&
+      <View style={styles.popup}>
+        <Text style={styles.popup.text}>{loginError}</Text>
       </View>
+    }
+    <Text style={styles.h2}>Login to the Motivation Scale</Text>
 
-      <Link to={{screen: "Signup"}} style={styles.link}>Don't have an account? Sign up</Link>
-    </SafeAreaView>
+    <View style={styles.form}>
+      <Text style={styles.text}>Email</Text>
+      <TextInput
+        style={styles.textInput}
+        onFocus={()=>setLoginError(undefined)}
+        onChangeText={email=>{
+          setLoginError(undefined)
+          onChange("email", email)
+        }}
+        placeholder="Enter you email"
+        placeholderTextColor={variables.highlight}
+      />
+
+      <Text style={styles.text}>Password</Text>
+      <TextInput
+        secureTextEntry={true}
+        style={styles.textInput}
+        onFocus={()=>setLoginError(undefined)}
+        onChangeText={password=>{
+          setLoginError(undefined)
+          onChange("password", password)
+        }}
+        placeholder="Enter you password"
+        placeholderTextColor={variables.highlight}
+     />
+
+      <Button color={variables.highlight} title="Login" onPress={onSubmit}/>
+    </View>
+
+    <Link to={{screen: "Signup"}} style={styles.link}>Don't have an account? Sign up</Link>
+  </SafeAreaView>
   )
 }
 
@@ -84,5 +109,18 @@ const styles = StyleSheet.create({
   form: {
     width: 300,
     maxWidth: Dimensions.get("window").width * 0.9
+  },
+  popup: {
+    borderWidth: 1,
+    borderColor: 'red',
+    borderRadius: 5,
+    padding: 8,
+    position: 'absolute',
+    top: 20,
+
+    text: {
+      color: 'pink',
+      fontSize: 18,
+    }as const
   }
 })
