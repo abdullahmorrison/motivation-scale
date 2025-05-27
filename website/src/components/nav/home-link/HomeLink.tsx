39d9b2163131b0648efeacd9217d37948115ffc1
Abdullah Morrison <abdullahmorrison@gmail.com>
2025-05-27 12:55:43 -0400

    refactored nav homelink

--- website/src/components/nav/home-link/HomeLink.tsx
+++ website/src/components/nav/home-link/HomeLink.tsx
@@ -9,18 +9,15 @@ export default function HomeLink(){
   const pathname = usePathname()
 
   return (
-    <div>
-      <Link href={routes.home}>
+    <Link href={routes.home}>
       {pathname === routes.home ?
-        <Link href={routes.home}>
-          <h3 className={styles.logo}><Image src="/icons/logo.svg" alt='logo' width={40} height={40}/>
-            Motivation Scale
-          </h3>
-        </Link>
-      :
+        <h3 className={styles.logo}>
+          <Image src="/icons/logo.svg" alt='logo' width={40} height={40}/>
+          <span>Motivation Scale</span>
+        </h3>
+        :
         <Image src="/icons/logo.svg" width={75} height={75} alt="logo"/>
       }
-      </Link>
-    </div>
+    </Link>
   )
 }
